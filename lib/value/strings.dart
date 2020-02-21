aa801eaf2087d5818ae3b1e8762374db81d250c0
Nguyễn Hoàng Thiện <hoangthienvn97@gmail.com>
2020-02-21 22:57:06 +0700

    update

--- lib/value/strings.dart
+++ lib/value/strings.dart
@@ -11,4 +11,8 @@ class Strings {
   static final String login_pwd_pwd = 'Mật Khẩu';
   static final String login_pwd_forget_pwd = 'Quên mật khẩu';
   static final String login_pwd_signin = 'ĐĂNG NHẬP';
+  static final String pwd_field_pwd ='Nhập mật khẩu';
+  static final String pwd_field_pwd_agian ='Nhập lại mật khẩu';
+  static final String login_pwd_notification ='THÔNG BÁO';
+  static final String login_pwd_err_pwd ='Sai mật khẩu đăng nhập';
 }
