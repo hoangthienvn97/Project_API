d14df22d5d376cbf69b3bd1555dbbb83d4e0211e
Nguyễn Hoàng Thiện <hoangthienvn97@gmail.com>
2020-02-21 12:02:22 +0700

    done fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'login_page.dart';
-import 'login_password.dart';
-import 'sign_up.dart';
-import 'foreign_password.dart';
-import './company/select_company.dart';
-import './company/create_company.dart';
 import './company_infor/checkIn_action.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final routes = <String,WidgetBuilder>{
-    LoginPage.tag: (context) =>LoginPage(),
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
   };
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,12 +18,10 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
-        const Locale('en','US')
-      ],
+      supportedLocales: [const Locale('en', 'US')],
       title: 'Ahihi code funny',
       debugShowCheckedModeBanner: false,
-      home: KeyValuePairDropdown(),
+      home: LoginPage(),
       routes: routes,
     );
   }
