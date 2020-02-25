97273bcaf21062dbd95f328cc355f2423241434f
Nguyễn Hoàng Thiện <hoangthienvn97@gmail.com>
2020-02-25 10:56:51 +0700

    fix done

--- lib/common/pwd_field.dart
+++ lib/common/pwd_field.dart
@@ -10,16 +10,19 @@ class PwdField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: TextFormField(
-      autofocus: false,
-      obscureText: true,
-      controller: controller,
-      decoration: InputDecoration(
-        hintText: type == PwdType.pwd ? Strings.pwd_field_pwd : Strings.pwd_field_pwd_agian,
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
-    ));
+      child: TextFormField(
+        autofocus: false,
+        obscureText: true,
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: type == PwdType.pwd
+            ? Strings.pwd_field_pwd
+            : Strings.pwd_field_pwd_agian,
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        ),
+      )
+    );
   }
 }
 
