7e2865ce4425ba363b8047a22ede0d1fa9a7561d
Nguyễn Hoàng Thiện <hoangthienvn97@gmail.com>
2020-03-03 23:08:06 +0700

    add

---
+++ lib/common/config.dart
@@ -0,0 +1,5 @@
+class UrlList{
+  static final String url =
+        'https://auth-dev.wisami.com/oauth2/authorize?response_type=code&redirect_uri=https://auth-dev.wisami.com/oauth2/code&client_id=wisami_base_1522119499';
+  static final String url_SignUp = 'https://auth-dev.wisami.com/user';      
+}
